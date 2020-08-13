33ff27c8277743074c3b9b55b0ebe271afe7904d
Neppa97 <lorenzoappendini@gmail.com>
2020-08-13 10:40:31 +0200

    added filewatcher_tests

---
+++ filewatcher_tests/README.md
@@ -0,0 +1,9 @@
+# Filewatcher_test
+
+Dopo aver eseguito il programma, si può testare aprendo terminale dalla cartella fw_test ed eseguendo comandi come ad esempio:
+
+- touch t1
+- echo "test modifica" > t1
+- rm t1
+
+OS: Linux Mint 19.3 ver 4.4.8
---
+++ filewatcher_tests/file_watcher/cmake-build-debug/CMakeFiles/file_watcher.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/neppa/Scrivania/m1_project/tests/file_watcher/main.cpp" "/home/neppa/Scrivania/m1_project/tests/file_watcher/cmake-build-debug/CMakeFiles/file_watcher.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
